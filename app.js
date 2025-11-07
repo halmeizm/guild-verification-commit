7bf97313eb82d4c739382be91f43c8e17a87dcb8
halmeizm <154517820+halmeizm@users.noreply.github.com>
2025-11-07 19:52:21 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:33:40.954Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-07T16:52:13.610Z
+Commit: Fix CSS styling bug on mobile devices
